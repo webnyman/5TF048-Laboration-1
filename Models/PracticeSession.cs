8e01d154ba33f6c03c26a006a4abee2261a7aca4
Björn Nyman <bjorn.nyman@webflex.se>
2025-09-26 10:05:38 +0200

    Add PracticeSession model

---
+++ Models/PracticeSession.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticeLogger.Models
+{
+    public class PracticeSession
+    {
+        public int SessionId { get; set; }
+
+        [Required]
+        public int InstrumentId { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        public DateTime PracticeDate { get; set; } = DateTime.Today;
+
+        [Range(1, 600, ErrorMessage = "Minuter 1–600.")]
+        public int Minutes { get; set; }
+
+        [Range(1, 5, ErrorMessage = "Intensitet 1–5.")]
+        public byte Intensity { get; set; }
+
+        [Required, StringLength(200)]
+        public string Focus { get; set; } = "";
+
+        public string? Comment { get; set; }
+    }
+
+    public class Instrument { public int InstrumentId { get; set; } public string Name { get; set; } = ""; }
+
+}
