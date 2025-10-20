f970e1e3bf69dfbde3ae3d86c7b84b637f946237
Björn Nyman <bjorn.nyman@webflex.se>
2025-10-20 09:34:57 +0200

    Add fields to views

---
+++ Models/PracticeTypeCatalog.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeLogger.Models
+{
+    public class PracticeTypeCatalog : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
